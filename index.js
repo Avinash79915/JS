a924aae3f4a029091051ecf34246f5967d6c4118
Avinash79915 <avikushwaha121@gmail.com>
2023-05-05 23:30:28 +0530

    update 1

---
+++ index.js
@@ -0,0 +1,94 @@
+/*var avinash = 'b';
+if(avinash=='a'){
+    console.log('hello jii')
+}
+else{
+    console.log('BYE JII');
+}
+
+var a=2004;
+if(a%4==0){
+    console.log('leap year');
+}
+else{
+    console.log('not a leap year')
+}
+
+var num = 0;
+while(num<=10){
+    console.log(num);
+    num++;
+}
+function add(){
+    var a=10;
+    var b=20;
+    var c=a+b;
+    console.log(c);
+}
+function sub(){
+    var a=10;
+    var b=20;
+    var c=a-b;
+    console.log(c);
+    
+}
+
+add();
+function add(a,b){
+    var total=a+b;
+    console.log(total);
+    
+}
+add(20,30);
+add(40,30);
+add(90,30);
+
+
+for(let num = 1; num<=10; num++){
+let table=12;
+//console.log(table +"*" + num +"=" + table*num);
+console.log(`${table} * ${num} = ${table*num}`);
+
+}
+
+
+function sum(){
+    let a=10 ,b=20;
+    let c=a+b;
+    return `sum is ${c}`;
+    
+}
+
+const sum = () =>{
+    let a=10 ,b=20;
+    let c=a+b;
+    return `sum is ${c}`; 
+} 
+console.log(sum());
+
+const sum = () =>{
+   // let a=10 ,b=20;
+    //let c=a+b;
+    return `sum is ${(a=10)+(b=20)}`; 
+} 
+
+console.log(sum());             
+*/
+
+// const sum = () =>`sum is ${(a=10)+(b=20)}`; 
+//  console.log(sum()); 
+
+
+var avi=["avinashj","kushwhaha","monu","Beast"];
+console.log(avi[3]);
+ 
+console.log(avi.length);
+
+
+
+
+
+
+
+
+
